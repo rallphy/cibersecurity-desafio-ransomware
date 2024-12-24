c001210e9547250bda5abe74c50552ccb929d656
Ralphy <106680151+rallphy@users.noreply.github.com>
2024-12-24 17:06:28 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,8 +1,16 @@
 import os
 import pyaes
+import glob
+
+## encontrar o arquivo com a extensão .ransom
+files = glob.glob("*.ransom")
+if not files:
+    print("Nenhum arquivo .ransom encontrado.")
+    exit()
+
+file_name = files[0]
 
 ## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
@@ -16,7 +24,7 @@ decrypt_data = aes.decrypt(file_data)
 os.remove(file_name)
 
 ## criar o arquivo descriptografado
-new_file = "teste.txt"
+new_file = file_name.replace(".ransom", "")
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
 new_file.close()
